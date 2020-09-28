ef9deee64d8dc3a8de2f3f5bafb4d119f073eaa8
David Vanderburgh <david.vanderburgh@cardinalhealth.com>
2020-09-27 22:31:40 -0400

    feat: vending-machine stubbed

--- my-app/src/main/java/com/fuse/app/App.java
+++ my-app/src/main/java/com/fuse/app/App.java
@@ -2,22 +2,26 @@ package com.fuse.app;
 
 public class App 
 {
-  public static int add(int firstNumber, int secondNumber) {
-    return firstNumber + secondNumber;
+  public static int nickelsInserted = 0;
+  public static int dimesInserted = 0;
+  public static int quartersInserted = 0;
+
+  public static void insertCoin (String coinType) {
+    switch (coinType) {
+      case "nickel":
+        nickelsInserted++;
+        break;
+      case "dime":
+        dimesInserted++;
+        break;
+      case "quarter":
+        quartersInserted++;
+        break;
+    }
   }
 
-  public static int multiply(int multiplicand, int multiplier) {
-    return multiplicand * multiplier;
-  }
-
-  public static int divide(int dividend, int divisor) {
-    if (divisor == 0)
-      throw new IllegalArgumentException("Cannot divide by zero (0).");
-
-    return dividend / divisor;
-  }
-  public static void main( String[] args )
+  public static void main (String[] args)
   {
-    System.out.println(App.add(3, 3)); 
+    System.out.println("something goes here eventually");
   }
 }
--- my-app/src/test/java/com/fuse/app/AppTest.java
+++ my-app/src/test/java/com/fuse/app/AppTest.java
@@ -5,51 +5,19 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
     @Test
-    public void add_TwoPlusTwo_ReturnsFour() {
-      // Arrange
-      final int expected = 4;
-  
-      // Act
-      final int actual = App.add(2, 2);
-  
-      // Assert
-      Assert.assertEquals(expected, actual);
-    }    
-    @Test
-    public void multiply_FourTimesTwo_ReturnsEight() {
-      // Arrange
-      final int expected = 8;
-  
-      // Act
-      final int actual = App.multiply(4, 2);
-  
-      // Assert
-      Assert.assertEquals(expected, actual);
-    }        
+    public void insertCoin_nickel_oneNickel() {
+      int expectedNickelCount = 1;
 
-    @Test
-    public void divide_TenDividedTwo_ReturnsFive() {
-      // Arrange
-      final int expected = 5;
+      App.insertCoin("nickel");
   
-      // Act
-      final int actual = App.divide(10, 2);
-  
-      // Assert
-      Assert.assertEquals(expected, actual);
-    }           
+      Assert.assertEquals(expectedNickelCount, App.nickelsInserted);
+    }    
 }

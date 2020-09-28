e039a56af1e3abcc51e44836751208fabfd96f8a
David Vanderburgh <david.vanderburgh@cardinalhealth.com>
2020-09-27 21:25:26 -0400

    feat: java boot-strap

---
+++ src/test/java/com/vending-machine-tests/VendingMachineTests.java
@@ -0,0 +1,10 @@
+class MyFirstJUnitJupiterTests {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void addition() {
+        assertEquals(2, calculator.add(1, 1));
+    }
+
+}
